c51c58d233028e77c9382b3a6efb55df504d0ee8
rufatZZ <rufat.zeinalli@gmail.com>
2019-01-28 00:04:51 +0400

    loading  added

--- src/App.js
+++ src/App.js
@@ -11,19 +11,28 @@ class App extends Component {
     this.state = {
       entry: "",
       wordList: [],
-      counter: 0
+      counter: 0,
+      isSearching: false,
+      isLoading: true,
+      isInvalid: false,
+      isEmpty: true
     };
     this.handleSearch = this.handleSearch.bind(this);
   }
 
   async handleSearch(word) {
+    this.setState({ isLoading: true, isSearching: true });
     this.setState({ entry: word });
     let wordListJSON = await getDefinitionByWord(word);
-    this.setState({ wordList: wordListJSON });
+    this.setState({
+      wordList: wordListJSON,
+      isLoading: false,
+      isSearching: false
+    });
   }
 
   render() {
-    let { entry, wordList } = this.state;
+    let { entry, wordList, isLoading, isSearching } = this.state;
 
     return (
       <div className="container">
@@ -31,7 +40,14 @@ class App extends Component {
         <hr />
         <div className="row">
           <div className="offset-md-2 col-md-8">
-            <WordList wordList={wordList} entry={entry} />
+            {isLoading && isSearching && (
+              <div className="alert alert-primary text-center">
+                <h5>Loading...</h5>
+              </div>
+            )}
+            {!isLoading && !isSearching && (
+              <WordList wordList={wordList} entry={entry} />
+            )}
           </div>
         </div>
       </div>
