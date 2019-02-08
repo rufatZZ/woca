2314c61a3dbf6428d2f3757a79f45f4ab2bd5db7
rufatZZ <rufat.zeinalli@gmail.com>
2019-02-09 01:10:21 +0400

    breaking changes for main layout

--- src/App.js
+++ src/App.js
@@ -6,31 +6,45 @@ import Header from "./components/Header";
 
 import Sidebar from "./components/Sidebar";
 import History from "./components/History/History";
+
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  margin: 0 auto;
+`;
+
+const ContentHolder = styled.div`
+  padding: 35px 0 0;
+`;
+
 class App extends Component {
   render() {
     return (
       <div>
         <Router>
-          <div className="container-fluid">
-            <Header />
+          <Container>
+            {/* <Header /> */}
             <div className="row">
-              <div className="col-md-3">
+              <div className="col-md-3" style={{ position: "relative" }}>
                 <Sidebar />
               </div>
               <div className="col-md-9">
-                <div className="container">
-                  <Route
-                    exact
-                    path="/"
-                    render={() => <Redirect to="/search" />}
-                  />
-                  <Route exact path="/search" component={Search} />
-                  <Route path="/search/:word" component={Search} />
-                  <Route path="/history" component={History} />
-                </div>
+                <ContentHolder>
+                  <Container>
+                    <Route
+                      exact
+                      path="/"
+                      render={() => <Redirect to="/search" />}
+                    />
+                    <Route exact path="/search" component={Search} />
+                    <Route path="/search/:word" component={Search} />
+                    <Route path="/history" component={History} />
+                  </Container>
+                </ContentHolder>
               </div>
             </div>
-          </div>
+          </Container>
         </Router>
       </div>
     );
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,35 +3,77 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const SidebarLink = styled(Link)`
-  color: black;
-  border-radius: 0px!important;
-  &:first-child {
-    border-top-right-radius: 3px !important;
-    border-top-left-radius: 3px !important;
-  }
-  &:last-child {
-    border-bottom-right-radius: 3px !important;
-    border-bottom-left-radius: 3px !important;
-  }
-  &:hover {
+  font-size: 16px;
+  color: white;
+  padding: 10px 10px;
+  border-radius: 2px;
+  &:hover,
+  &:focus {
+    outline: none !important;
+    text-decoration: none !important;
     color: white;
-    background-color: #007bff;
+    background-color: #11328d;
+    transition: 0.15s ease-in-out;
   }
+`;
+
+const SidebarWrapper = styled.div`
+  position: fixed;
+  width: 20%;
+  height: 100vh;
+  background-color: #11369e;
+  padding: 25px 25px;
+`;
+
+const SidebarBrand = styled.div`
+  color: white;
+  margin: 0px 0px 40px;
+`;
+
+const SidebarLinkListHolder = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+const SidebarLinkList = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 30px;
+`;
+
+const SidebarLinkListTitle = styled.span`
+  color: white;
+  opacity: 0.4;
+  font-size: 14px;
 `;
 
 const Sidebar = () => {
   return (
-    <div className="list-group">
-      <SidebarLink to="/" className="list-group-item">
-        Search
-      </SidebarLink>
-      <SidebarLink to="/history" className="list-group-item">
-        History
-      </SidebarLink>
-      <SidebarLink to="/" className="list-group-item">
-        Saved
-      </SidebarLink>
-    </div>
+    <SidebarWrapper>
+      <SidebarBrand>
+        <h1>Woca</h1>
+      </SidebarBrand>
+
+      <SidebarLinkListHolder>
+        <SidebarLinkList>
+          <SidebarLinkListTitle>Profile</SidebarLinkListTitle>
+          <SidebarLink to="/">My profile</SidebarLink>
+          <SidebarLink to="/">Notifications</SidebarLink>
+          <SidebarLink to="/">Settings</SidebarLink>
+        </SidebarLinkList>
+
+        <SidebarLinkList>
+          <SidebarLinkListTitle>Vocabulary</SidebarLinkListTitle>
+          <SidebarLink to="/">Search</SidebarLink>
+          <SidebarLink to="/history">History</SidebarLink>
+          <SidebarLink to="/">Saved</SidebarLink>
+        </SidebarLinkList>
+
+        <SidebarLinkList>
+          <SidebarLink to="/">Log out</SidebarLink>
+        </SidebarLinkList>
+      </SidebarLinkListHolder>
+    </SidebarWrapper>
   );
 };
 
