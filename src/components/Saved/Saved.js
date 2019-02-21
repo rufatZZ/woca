4960fd7a5e13008f178485bc6a1b18c9e2acc1d2
rufatZZ <rufat.zeinalli@gmail.com>
2019-02-22 00:12:34 +0400

    saved list component optimized

--- src/components/Saved/Saved.js
+++ src/components/Saved/Saved.js
@@ -2,53 +2,6 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { getAllSavedWords, deleteSavedWord } from "../../actions/actions";
 
-class SavedList extends Component {
-
-  constructor(props){
-    super(props);
-    this.handleDelete = this.handleDelete.bind(this);
-  }
-
-  handleDelete(word_id){
-    this.props.deleteWord(word_id);
-  }
-
-  render() {
-    const { wordList } = this.props;
-    let count = 0;
-    wordList.sort(function(a, b) {
-      return Date.parse(b.time) - Date.parse(a.time);
-    });
-    return wordList.map(word => {
-      count++;
-      return (
-        <tr key={word._id}>
-          <th scope="row" width="5%">
-            {count}
-          </th>
-          <td width="80%">{word.title}</td>
-          <td width="20%">
-            <Link
-              to={`/search?word=${word.title}`}
-              className="btn btn-outline-info"
-            >
-              Get definition
-            </Link>
-          </td>
-          <td width="5%">
-            <button
-              className="btn btn-outline-danger"
-              onClick={(e) => this.handleDelete(word._id)}
-            >
-              Delete
-            </button>
-          </td>
-        </tr>
-      );
-    });
-  }
-}
-
 class Saved extends Component {
   constructor(props) {
     super(props);
@@ -60,18 +13,18 @@ class Saved extends Component {
     this.getAllSavedWords = this.getAllSavedWords.bind(this);
     this.handleDeleteSavedWord = this.handleDeleteSavedWord.bind(this);
   }
-  
+
   componentWillMount() {
     this.getAllSavedWords();
   }
 
   async handleDeleteSavedWord(word_id) {
     let deleteResponse = await deleteSavedWord(word_id);
-    if(deleteResponse.isDeleted){
-      this.getAllSavedWords();      
+    if (deleteResponse.isDeleted) {
+      this.getAllSavedWords();
     }
   }
-  
+
   async getAllSavedWords() {
     let savedList = await getAllSavedWords();
     this.setState({ savedList: savedList });
@@ -79,6 +32,11 @@ class Saved extends Component {
 
   render() {
     let { savedList } = this.state;
+    let count = 0;
+    savedList.sort(function(a, b) {
+      return Date.parse(b.time) - Date.parse(a.time);
+    });
+
     return (
       <div>
         <div className="row">
@@ -88,10 +46,33 @@ class Saved extends Component {
         <div className="row">
           <table className="table table-hover table-borderless">
             <tbody>
-              <SavedList
-                wordList={savedList}
-                deleteWord={this.handleDeleteSavedWord}
-              />
+              {savedList.map(word => {
+                count++;
+                return (
+                  <tr key={word._id}>
+                    <th scope="row" width="5%">
+                      {count}
+                    </th>
+                    <td width="80%">{word.title}</td>
+                    <td width="20%">
+                      <Link
+                        to={`/search?word=${word.title}`}
+                        className="btn btn-outline-info"
+                      >
+                        Get definition
+                      </Link>
+                    </td>
+                    <td width="5%">
+                      <button
+                        className="btn btn-outline-danger"
+                        onClick={e => this.handleDelete(word._id)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
             </tbody>
           </table>
         </div>
