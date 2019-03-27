beae0963a0d080fdb0b4cbcca6799de35c7a9da9
rufatZZ <rufat.zeinalli@gmail.com>
2019-03-27 22:23:52 +0400

    Some re-factoring

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
-import styled, { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from "styled-components";
 
-import Search from "./components/Search/Search";
-import Sidebar from "./components/Sidebar";
-import History from "./components/History/History";
-import Saved from "./components/Saved/Saved";
-
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/free-solid-svg-icons";
-
-library.add(fas);
+import Woca from "./components/Woca";
 
 const GlobalStyle = createGlobalStyle`
    body {
@@ -18,28 +9,6 @@ const GlobalStyle = createGlobalStyle`
       margin: 0;
       padding: 0;
    }
-
-`;
-
-const Container = styled.div`
-  max-width: 100%;
-  margin: 0 auto;
-`;
-
-const ContentHolder = styled.div`
-  padding: 35px 0 0;
-`;
-
-const Flex = styled.div`
-  display: flex;
-`;
-
-const SidebarWrapper = styled.div`
-  width: 300px;
-`;
-
-const ContentWrapper = styled.div`
-  width: calc(100% - 400px);
 `;
 
 class App extends Component {
@@ -47,30 +16,7 @@ class App extends Component {
     return (
       <React.Fragment>
         <GlobalStyle />
-          <Router>
-            <Container>
-              <Flex>
-                <SidebarWrapper>
-                  <Sidebar />
-                </SidebarWrapper>
-                <ContentWrapper>
-                  <ContentHolder>
-                    <Container>
-                      <Route
-                        exact
-                        path="/"
-                        render={() => <Redirect to="/search" />}
-                      />
-                      <Route exact path="/search" component={Search} />
-                      <Route path="/search/:word" component={Search} />
-                      <Route path="/history" component={History} />
-                      <Route path="/saved" component={Saved} />
-                    </Container>
-                  </ContentHolder>
-                </ContentWrapper>
-              </Flex>
-            </Container>
-          </Router>
+        <Woca />
       </React.Fragment>
     );
   }
---
+++ src/components/Woca.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import styled from "styled-components";
+
+import Sidebar from "./_partials/Sidebar";
+import Search from "./Search/Search";
+import History from "./History/History";
+import Saved from "./Saved/Saved";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+
+library.add(fas);
+
+const Container = styled.div`
+  max-width: 100%;
+  margin: 0 auto;
+`;
+
+const ContentHolder = styled.div`
+  padding: 35px 0 0;
+`;
+
+const Flex = styled.div`
+  display: flex;
+`;
+
+const SidebarWrapper = styled.div`
+  width: 300px;
+`;
+
+const ContentWrapper = styled.div`
+  width: calc(100% - 400px);
+`;
+
+class Woca extends Component {
+  render() {
+    return (
+      <Router>
+        <Container>
+          <Flex>
+            <SidebarWrapper>
+              <Sidebar />
+            </SidebarWrapper>
+            <ContentWrapper>
+              <ContentHolder>
+                <Container>
+                  <Route
+                    exact
+                    path="/"
+                    render={() => <Redirect to="/search" />}
+                  />
+                  <Route exact path="/search" component={Search} />
+                  <Route path="/search/:word" component={Search} />
+                  <Route path="/history" component={History} />
+                  <Route path="/saved" component={Saved} />
+                </Container>
+              </ContentHolder>
+            </ContentWrapper>
+          </Flex>
+        </Container>
+      </Router>
+    );
+  }
+}
+
+export default Woca;
