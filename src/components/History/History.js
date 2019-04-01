b18638e4e7e47e063b7df859e4c4f65998f422da
rufatZZ <rufat.zeinalli@gmail.com>
2019-04-01 23:19:30 +0400

    History component re-styled

--- src/components/History/History.js
+++ src/components/History/History.js
@@ -1,7 +1,63 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import { getAllHistory } from "../../actions/actions";
 
+const SavedTitle = styled.h1`
+  font-family: "MS-Bold";
+  font-size: 2.5rem;
+`;
+
+const SavedFlex = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const SavedWordBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+  height: 200px;
+  padding: 20px;
+  margin: 0px 25px 30px 0px;
+  border-radius: 1rem;
+  box-sizing: border-box;
+  box-shadow: -10px 10px 20px 0px rgb(222, 221, 221);
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    ${"" /* cursor: pointer; */}
+    box-shadow: -10px 10px 20px 0px #994ed361;
+    transition: all 0.2s ease-in-out;
+  }
+`;
+
+const SavedWordBoxTitle = styled.h2`
+  text-transform: capitalize;
+  font-size: 1.5rem;
+  font-family: "MS-Bold";
+  opacity: 0.6;
+`;
+
+const SavedWordBoxBody = styled.div`
+  height: 90%;
+`;
+
+const SavedWordBoxFooter = styled.div`
+  height: 10%;
+  font-size: 1rem;
+`;
+
+const SavedWordBoxFooterIcon = styled(FontAwesomeIcon)`
+  margin-right: 10px;
+  opacity: 0.6;
+  &:hover {
+    cursor: pointer;
+    opacity: 0.9;
+  }
+`;
 class History extends Component {
   constructor(props) {
     super(props);
@@ -33,7 +89,7 @@ class History extends Component {
     return (
       <div>
         <div className="row">
-          <h2>
+          <SavedTitle>
             <button
               className="btn btn-danger mr-3 mb-1"
               onClick={this.handleFlush}
@@ -41,34 +97,41 @@ class History extends Component {
               <i className="fa fa-trash" /> Flush
             </button>
             History <small>(*session based history)</small>
-          </h2>
+          </SavedTitle>
         </div>
         <br />
-        <div className="row">
-          <table className="table table-hover table-borderless">
-            <tbody>
-              {historyList.map(word => {
-                return (
-                  <tr key={Math.random() * Date.parse(word.time)}>
-                    <td>{word.time}</td>
-                    <td>{word.value}</td>
-                    <td width="20%">
-                      <Link
-                        to={`/search?word=${word.value}`}
-                        className="btn btn-outline-info"
-                      >
-                        Get definition
-                      </Link>
-                    </td>
-                    <td width="5%">
-                      <button className="btn btn-outline-danger">Delete</button>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
+        <SavedFlex>
+          {historyList.map(word => {
+            return (
+              <SavedWordBox key={Math.random() * Date.parse(word.time)}>
+                <SavedWordBoxBody>
+                  <SavedWordBoxTitle>
+                    {word.value}
+                  </SavedWordBoxTitle>
+                </SavedWordBoxBody>
+                <SavedWordBoxFooter>
+                  <SavedWordBoxFooterIcon
+                    icon="trash-alt"
+                    title="Delete"
+                    // onClick={e => this.handleDeleteSavedWord(word._id)}
+                  />
+                  <Link
+                    to={`/search?word=${word.value}`}
+                    style={{ color: "black" }}
+                  >
+                    <SavedWordBoxFooterIcon
+                      icon="external-link-square-alt"
+                      title="Get definition"
+                      style={{ transform: "translateY(8%)" }}
+                    />
+                  </Link>
+                  {word.time}
+
+                </SavedWordBoxFooter>
+              </SavedWordBox>
+            );
+          })}
+        </SavedFlex>
       </div>
     );
   }
