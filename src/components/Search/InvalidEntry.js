c08798b758079dda530201765a6183d123ad6e7f
rufatZZ <rufat.zeinalli@gmail.com>
2019-04-20 20:18:15 +0400

    Row, Container, Col custom components initialized

--- src/components/Search/InvalidEntry.js
+++ src/components/Search/InvalidEntry.js
@@ -3,7 +3,7 @@ import WAlert from '../../toolbox/components/Alert';
 
 const InvalidEntry = ({entry}) => {
   return (
-    <WAlert bg="danger" className="alertalert-warning text-center mb-3">
+    <WAlert bg="danger" className="text-center mb-3">
       <h4>"{entry}"</h4>
       <p>
         The word you've entered isn't in the dictionary. Click on a spelling
