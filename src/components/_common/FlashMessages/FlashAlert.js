f7d86eebd3f18f5f9aabb75426c42cbb7d2ce48a
rufatZZ <rufat.zeinalli@gmail.com>
2019-07-24 23:23:36 +0400

    flashmessage close icon updated

--- src/components/_common/FlashMessages/FlashAlert.js
+++ src/components/_common/FlashMessages/FlashAlert.js
@@ -1,6 +1,18 @@
 import React, { Component } from "react";
-import { Alert, Button } from "../../../toolbox/components";
+import styled from 'styled-components';
+import { Alert } from "../../../toolbox/components";
 
+const FlasAlertItemRemoveIcon = styled.div`
+  background-image: url(data:image/svg+xml;base64,PHN2ZyBoZWlnaHQ9IjE4cHgiIHdpZHRoPSIxOHB4IiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxOCAxOCIgZmlsbD0iIzAwMDAwMCI+CiA8cGF0aCBkPSJtMCAwaDE4djE4aC0xOHpoMTh2MThoLTE4eiIgZmlsbD0ibm9uZSIvPgogPHBhdGggZD0ibTE0LjUzIDQuNTNsLTEuMDYtMS4wNi00LjQ3IDQuNDctNC40Ny00LjQ3LTEuMDYgMS4wNiA0LjQ3IDQuNDctNC40NyA0LjQ3IDEuMDYgMS4wNiA0LjQ3LTQuNDcgNC40NyA0LjQ3IDEuMDYtMS4wNi00LjQ3LTQuNDd6Ii8+Cjwvc3ZnPgo=);
+  display: inline-block;
+  width: 15px;
+  height: 15px;
+  position: relative;
+  top: 2px;
+  margin-right: 5px;
+  background-repeat: no-repeat;
+  background-size: 13px 15px;
+`;
 class FlashAlert extends Component {
   componentDidMount() {
     this.timer = setTimeout(this.props.onClose.bind(this), this.props.timeout);
@@ -28,13 +40,9 @@ class FlashAlert extends Component {
         className="animated fadeInUp"
         style={{ marginTop: "5px" }}
       >
-        <Button
-          bg="glass"
-          data-dismiss="alert"
-          onClick={e => onClose(message.id)}
-        >
-          &times;
-        </Button>
+
+        <FlasAlertItemRemoveIcon onClick={e => onClose(message.id)} />
+
         {message.text}
       </Alert>
     );
